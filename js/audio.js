09b83132b793b389af30a93ecdfce5ff7441d852
De'Andre <hd2319@gmail.com>
2015-12-11 20:16:44 -0800

    Added audio init starter stuff

---
+++ js/audio.js
@@ -0,0 +1,50 @@
+function AudioHandler() {
+
+    this.init = function() {
+        //Initialize whole handler, load requested resources
+        this.audioData = new Array()
+        this.initContext()
+    }
+    
+    this.initContext = function() {
+        
+        try {
+            // Create an audio context
+            window.AudioContext = window.AudioContext || window.webkitAudioContext;
+            this.context = new AudioContext();
+        }
+        catch(e) {
+            console.log('[!] Failed to initalize audio context!'
+        }
+    }
+    
+    this.loadAudio( resources ) {
+        for(  target in resources ) {
+            console.log("[=] Audio init: Loading " + target)
+            var req = XMLHttpRequest();
+            req.open('GET' , target, true)
+            req.responseType = 'arrayBuffer';
+            
+            req.onload = function() {
+                console.log("[=] Audio init: Got Response from " + target}
+                try {
+                    this.context.decodeAudioData(req.response, function(buf) {
+                            this.audioData.push(buf)
+                    }
+                except (ex) {
+                    console.log('[!] Caught Exception processing audio')
+                }
+            }
+            req.send();
+        }
+        var request = XMLHttpRequest();
+    }
+    
+    this.playSound( index ) {
+        var source = this.context.createBufferSource();
+        source.buffer = this.audioData(index);
+        source.connect(this.context.destination);
+        source.start(0);
+    }
+}
+    
